0524a475468e0ee6438aa3ce29de213c2e95fb79
David Puerto Guerrero <dsp5502@gmail.com>
2023-09-06 22:26:54 -0500

    connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { dbConnectPostgres } = require('./config/postgres');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port);
+
+dbConnectPostgres();
---
+++ src/config/postgres.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require('sequelize');
+
+const database = process.env.POSTGRES_DATABASE;
+const username = process.env.POSTGRES_USER;
+const password = process.env.POSTGRES_PASSWORD;
+const host = process.env.POSTGRES_HOST;
+
+console.log(database, username, password, host);
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect: 'postgres',
+  port: 5432,
+});
+
+const dbConnectPostgres = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+module.exports = {
+  sequelize,
+  dbConnectPostgres,
+};
